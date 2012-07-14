611427a217c0ed24b1c0e87975dab66cdd7ce1be
Bruce Beare <bruce.j.beare@intel.com>
2012-07-13 20:49:35 -0700

    build: make various subsystems selectable
    BZ: 46710
    
    For bringup and support of various hardware, we need to be able to disable
    intel add-on's in a convenient way via BoardConfig.mk. This commit adds
    required fixes to various Android.mk files.
    
    BoardConfig.mk is also changed disable a number of subsystems for bring-up
    of JB
    
    Change-Id: Id976736d07966e8948256ed462ad148246a05bbb
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Reviewed-on: http://android.intel.com:8080/56382
    Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,8 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
+ifeq ($(INTEL_VA),true)
+
+include $(CLEAR_VARS)
 VENDORS_INTEL_MRST_LIBMIX_ROOT := $(LOCAL_PATH)
 
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/mix_common/src/Android.mk
@@ -10,3 +12,5 @@ include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/mix_vbp/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/asfparser/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/videodecoder/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/videoencoder/Android.mk
+
+endif
