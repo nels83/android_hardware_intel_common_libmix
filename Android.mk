ecc50ef31841747dcb001a1434ee0177f8ef421e
wfeng6 <wei.feng@intel.com>
2012-09-21 18:53:27 +0800

    Remove unused codes and config files in video middleware stack
    BZ: 58358
    
    Remove unused codes and config files in video middleware stack.
    
    Change-Id: I9a3d9f4400a0cd6835d659c2cfdd35409094210f
    Signed-off-by: wfeng6 <wei.feng@intel.com>
    Reviewed-on: http://android.intel.com:8080/67297
    Reviewed-by: Xiao, FengX <fengx.xiao@intel.com>
    Reviewed-by: Liu, BolunX <bolunx.liu@intel.com>
    Reviewed-by: Zhao, Leo <leo.zhao@intel.com>
    Reviewed-by: Wang, Yi A <yi.a.wang@intel.com>
    Reviewed-by: Guo, Nana N <nana.n.guo@intel.com>
    Reviewed-by: Tang, Richard <richard.tang@intel.com>
    Reviewed-by: Qiu, Junhai <junhai.qiu@intel.com>
    Reviewed-by: Chen, Tianmi <tianmi.chen@intel.com>
    Reviewed-by: Gu, Wangyi <wangyi.gu@intel.com>
    Reviewed-by: Sun, Mingruo <mingruo.sun@intel.com>
    Reviewed-by: Shi, PingX <pingx.shi@intel.com>
    Tested-by: Shi, PingX <pingx.shi@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- Android.mk
+++ Android.mk
@@ -4,10 +4,6 @@ ifeq ($(INTEL_VA),true)
 
 include $(CLEAR_VARS)
 VENDORS_INTEL_MRST_LIBMIX_ROOT := $(LOCAL_PATH)
-
-include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/mix_common/src/Android.mk
-#include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/mix_audio/src/Android.mk
-include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/mix_video/src/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/mix_vbp/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/asfparser/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/videodecoder/Android.mk
@@ -16,4 +12,5 @@ include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/asf_extractor/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/vavideodecoder/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/libI420colorconvert/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/videoedit/Android.mk
+
 endif
