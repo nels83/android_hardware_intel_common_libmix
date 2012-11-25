acad5b82097247e4e4b36fc41679a43b48658da1
Dan Liang <dan.liang@intel.com>
2012-11-25 15:12:48 +0800

    [JB MR1] Revert patch for disabling asf extractor and video editor in libmix
    BZ: 69200
    
    Enable asf extractor
    
    Change-Id: Ie6ba75490b27d8ceeabb6569a4548762ede8396c
    Signed-off-by: Dan Liang <dan.liang@intel.com>
    Reviewed-on: http://android.intel.com:8080/75860
    Tested-by: Tong, BoX <box.tong@intel.com>
    Reviewed-by: Tong, BoX <box.tong@intel.com>

--- Android.mk
+++ Android.mk
@@ -5,10 +5,10 @@ ifeq ($(INTEL_VA),true)
 include $(CLEAR_VARS)
 VENDORS_INTEL_MRST_LIBMIX_ROOT := $(LOCAL_PATH)
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/mix_vbp/Android.mk
-#include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/asfparser/Android.mk
+include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/asfparser/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/videodecoder/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/videoencoder/Android.mk
-#include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/asf_extractor/Android.mk
+include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/asf_extractor/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/vavideodecoder/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/libI420colorconvert/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/videoedit/Android.mk
