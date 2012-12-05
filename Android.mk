81b888ea6f59f99ef8628e222e75584ffa5f674f
wfeng6 <wei.feng@intel.com>
2012-12-06 05:19:56 +0800

    Move video framework enhancement codes to libmedia_utils
    BZ: 72915
    
    Move video framework enhancement codes from libmix to
    libmedia_utils folder.
    
    Change-Id: I022033be9636b3ff2a0855309bd7337edd53f384
    Signed-off-by: wfeng6 <wei.feng@intel.com>
    Reviewed-on: http://android.intel.com:8080/79222
    Reviewed-by: Tang, Richard <richard.tang@intel.com>
    Reviewed-by: Ding, Haitao <haitao.ding@intel.com>
    Tested-by: Ding, Haitao <haitao.ding@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- Android.mk
+++ Android.mk
@@ -8,9 +8,5 @@ include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/mix_vbp/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/asfparser/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/videodecoder/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/videoencoder/Android.mk
-include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/asf_extractor/Android.mk
-include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/vavideodecoder/Android.mk
-include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/libI420colorconvert/Android.mk
-include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/frameworks/videoedit/Android.mk
 
 endif
