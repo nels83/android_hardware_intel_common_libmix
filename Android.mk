70b9f8248b167eb58ecd9ae8b3b8ced86b037f15
wfeng6 <wei.feng@intel.com>
2013-11-04 16:58:35 +0800

    To enable the optimized libmix parser code
    BZ: 149341
    
    This patch modifies the top-level Android.mk to
    enable the optimized libmix parser code.
    
    Change-Id: I027f36e55f6b476758203f25fee6d8aea0573c3c
    Signed-off-by: wfeng6 <wei.feng@intel.com>

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ ifeq ($(INTEL_VA),true)
 
 include $(CLEAR_VARS)
 VENDORS_INTEL_MRST_LIBMIX_ROOT := $(LOCAL_PATH)
-include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/mix_vbp/Android.mk
+include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/mixvbp/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/asfparser/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/videodecoder/Android.mk
 include $(VENDORS_INTEL_MRST_LIBMIX_ROOT)/videoencoder/Android.mk
