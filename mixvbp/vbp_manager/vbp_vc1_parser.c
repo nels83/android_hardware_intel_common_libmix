e1b2c72a5483c225c620c68c45c7ff3962597e7a
Tianmi Chen <tianmi.chen@intel.com>
2014-01-08 18:16:31 +0800

    libmix: A fix to set post-processing flag
    BZ: 161788
    
    post-processing flag should be parsed from sequence
    header in advanced profile.
    
    Change-Id: I2222dede26bb5344c075afaba7a63c1711df1729
    Signed-off-by: Tianmi Chen <tianmi.chen@intel.com>

--- mixvbp/vbp_manager/vbp_vc1_parser.c
+++ mixvbp/vbp_manager/vbp_vc1_parser.c
@@ -852,7 +852,7 @@ static void vbp_pack_picture_params_vc1(
     pic_parms->mb_mode_table = picLayerHeader->MBMODETAB;
     pic_parms->range_reduction_frame = picLayerHeader->RANGEREDFRM;
     pic_parms->rounding_control = picLayerHeader->RNDCTRL;
-    pic_parms->post_processing = picLayerHeader->POSTPROC;
+    pic_parms->post_processing = seqLayerHeader->POSTPROCFLAG;
     /* fix this.  Add RESPIC to parser.  */
     pic_parms->picture_resolution_index = 0;
     pic_parms->luma_scale = picLayerHeader->LUMSCALE;
