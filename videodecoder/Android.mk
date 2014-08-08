1b69a16ac4fa51c24b4be8f844a7253ed62cc710
Tarun Agarwal <tarun.agarwal@intel.com>
2014-08-08 13:35:59 -0700

    Fix a race in build for missing vbp_loader.h
    If the vbp_header.h doesn't copied then it was
    breaking videodecoder build. This fixes the race.
    
    Bug: 16897377
    
    Change-Id: I9d5da2eb0883ff8ad4ca9a4ed0414cc9a44a1be4

--- videodecoder/Android.mk
+++ videodecoder/Android.mk
@@ -70,6 +70,7 @@ endif
 LOCAL_CFLAGS += -Werror
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libva_videodecoder
+LOCAL_REQUIRED_MODULES :=libmixvbp
 
 ifeq ($(USE_HW_VP8),true)
 LOCAL_SRC_FILES += VideoDecoderVP8.cpp
