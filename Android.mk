08a885b961a991a5e383989e9ba28bc69ac8f729
Benoît Laniel <nels+git@pgroupe.net>
2015-05-19 21:14:12 +0200

    Include mixvbp
    Change-Id: I86ea4c92348ab61e289e903e6a5bbeda7d0a6a2c

--- Android.mk
+++ Android.mk
@@ -1,6 +1,7 @@
 AUDIO_PATH := $(call my-dir)
 
 ifeq ($(INTEL_VA),true)
+ include $(AUDIO_PATH)/mixvbp/Android.mk
  include $(AUDIO_PATH)/videodecoder/Android.mk
  include $(AUDIO_PATH)/videoencoder/Android.mk
 endif
